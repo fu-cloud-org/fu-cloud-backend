c14ee845bee8f5ccdeec7253d36ce29afea2097c
wendaidai <1106518291@qq.com>
2022-11-19 23:58:11 +0800

    fix

--- src/main/java/com/fucloud/fucloudbackend/service/impl/BmsFollowServiceImpl.java
+++ src/main/java/com/fucloud/fucloudbackend/service/impl/BmsFollowServiceImpl.java
@@ -1,8 +1,9 @@
-package com.fucloud.fucloudbackend.service;
+package com.fucloud.fucloudbackend.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.fucloud.fucloudbackend.model.entity.BmsFollow;
 import com.fucloud.fucloudbackend.dao.BmsFollowMapper;
+import com.fucloud.fucloudbackend.service.BmsFollowService;
 import org.springframework.stereotype.Service;
 
 @Service
